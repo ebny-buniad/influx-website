7f8e97fd4f7ac5d36e2353a70117b8ac098b80b0
ebny-buniad <ebnybuniad@gmail.com>
2024-07-25 12:48:52 +0600

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('header').addClass("sticky");
+        }
+        else {
+            $('header').removeClass("sticky");
+        }
+    });
+    // copyrights Year Auto-update
+    function newDate() {
+        return new Date().getFullYear();
+    }
+    document.onload = document.getElementById("autodate").innerHTML = + newDate();
+
+});
+
+
+AOS.init();
+AOS.refresh();
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:false,
+            loop:true
+        }
+    }
+})
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    /////// Prevent closing from click inside dropdown
+    document.querySelectorAll('.dropdown-menu').forEach(function (element) {
+        element.addEventListener('click', function (e) {
+            e.stopPropagation();
+        });
+    })
+});
