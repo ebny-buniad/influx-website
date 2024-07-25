0d0db0d21d82ecc5ee69fcee799b1c3c249c437b
ebny-buniad <ebnybuniad@gmail.com>
2024-07-26 00:12:10 +0600

    added products and footer

--- asset/js/main.js
+++ asset/js/main.js
@@ -24,17 +24,18 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     responsiveClass:true,
+    dots: false,
     responsive:{
         0:{
             items:1,
             nav:false
         },
         600:{
-            items:1,
+            items:3,
             nav:false
         },
         1000:{
-            items:1,
+            items:7,
             nav:false,
             loop:true
         }
