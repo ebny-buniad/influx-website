c55cb20f7703218816027692aa335b890e2ddb47
AtiqurRahman895 <emonhassan895@gmail.com>
2024-07-28 21:04:44 +0600

    ADDED 'borchuresPanel' SECTION IN 'applications.html', 'news.html', 'service.html' FILES

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,29 +1,43 @@
-const detailsTogglers= document.querySelectorAll('.detailsToggler')
-const details= document.querySelectorAll(".details")
-const viewDetails= document.querySelectorAll(".viewDetails")
-const HideDetails= document.querySelectorAll(".HideDetails")
-detailsTogglers.forEach((eachDetailsToggler,index)=>{
+const detailsTogglers = document.querySelectorAll(".detailsToggler");
+const details = document.querySelectorAll(".details");
+const viewDetails = document.querySelectorAll(".viewDetails");
+const HideDetails = document.querySelectorAll(".HideDetails");
+detailsTogglers.forEach((eachDetailsToggler, index) => {
   eachDetailsToggler.addEventListener("click", (e) => {
+    document.querySelector(`.details${index}`).classList.toggle("d-none");
 
-      document.querySelector(`.details${index}`).classList.toggle("d-none")
+    document.querySelector(`.viewDetails${index}`).classList.toggle("d-none");
 
-
-      document.querySelector(`.viewDetails${index}`).classList.toggle("d-none")
-
-
-      document.querySelector(`.HideDetails${index}`).classList.toggle("d-none")
-
+    document.querySelector(`.HideDetails${index}`).classList.toggle("d-none");
   });
+});
+
+
+// BROCHURES
+
+
+document.querySelector(".brochuresToggler").addEventListener("click", (e) => {
+  document.querySelector(".brochures").classList.toggle("brochuresAnimation");
+  document
+    .querySelector(".directionArrow")
+    .classList.toggle("directionArrowAnimation");
+});
+
+
+// BROCHURES panel
+
+const brochuresPanelToogler= document.querySelectorAll(`.brochuresPanelToogler`)
+
+brochuresPanelToogler.forEach((eachBrochuresPanelToogler)=>{
+  eachBrochuresPanelToogler.addEventListener('click',(e)=>{
+    console.log('working')
+    document.querySelector(`.brochuresPanel`).classList.toggle("brochuresPanelToggleAnimation")
+  })
+  
 })
 
 
-
-  document
-  .querySelector(".brochuresToggler")
-  .addEventListener("click", (e) => {
-    document.querySelector(".brochures").classList.toggle("brochuresAnimation");
-    document.querySelector(".directionArrow").classList.toggle("directionArrowAnimation");
-
-    
-  });
-
+const copyText= document.querySelector('.copyText')
+document.querySelector(`.copyButton`).addEventListener('click',(e)=>{
+  navigator.clipboard.writeText(copyText.value)
+})
