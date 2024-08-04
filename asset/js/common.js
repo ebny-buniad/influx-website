e799599b17823cec95f7211bdec46f4c8e70a3b7
AtiqurRahman895 <emonhassan895@gmail.com>
2024-08-04 18:16:57 +0600

    ADDED SEARCH PRODUCT FUNCTIONALITY IN THE HEADER SEARCH BAR OF EVERY PAGES

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,6 +1,29 @@
 
 console.log(window.location.pathname);
 
+// index page menu searchbar
+
+function filterFunction() {
+  let input, filter, ul, li, a, b, i;
+  input = document.querySelector(".search-bar");
+  filter = input.value.toUpperCase();
+  div = document.querySelector(".searchBarProductsWrapper");
+  a = div.getElementsByTagName("a");
+  b=document.querySelectorAll(".searchBarProductTitle")
+  for (i = 0; i < a.length; i++) {
+
+    let txtValue = b[i].innerText;
+    let lightType = b[i].title;
+    if (txtValue.toUpperCase().indexOf(filter) > -1 || lightType.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "flex";
+    } else {
+      a[i].style.display = "none";
+    }
+
+
+  }
+}
+
 // details toggling with index
 const detailsTogglers = document.querySelectorAll(".detailsToggler");
 const details = document.querySelectorAll(".details");
