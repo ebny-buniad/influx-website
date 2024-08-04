d78f7b16513c475768a0a1a2fb63865bb387a2f2
AtiqurRahman895 <emonhassan895@gmail.com>
2024-08-04 18:50:44 +0600

    MODIFIED HEADER SEARCH BAR IN EVERY PAGE

--- asset/js/common.js
+++ asset/js/common.js
@@ -3,25 +3,35 @@ console.log(window.location.pathname);
 
 // index page menu searchbar
 
-function filterFunction() {
-  let input, filter, ul, li, a, b, i;
-  input = document.querySelector(".search-bar");
-  filter = input.value.toUpperCase();
-  div = document.querySelector(".searchBarProductsWrapper");
-  a = div.getElementsByTagName("a");
-  b=document.querySelectorAll(".searchBarProductTitle")
+let input, filter, ul, li, a, b, i;
+input = document.querySelector(".search-bar");
+div = document.querySelector(".searchBarProductsWrapper");
+a = document.querySelectorAll(`.searchBarProduct`);
+b=document.querySelectorAll(".searchBarProductTitle")
+let searchResults=document.querySelector(`.searchResults`)
+searchResults.innerText=`Totall ${a.length}`
+
+function filterFunction() {  
+
+filter = input.value.toUpperCase();
   for (i = 0; i < a.length; i++) {
 
     let txtValue = b[i].innerText;
     let lightType = b[i].title;
     if (txtValue.toUpperCase().indexOf(filter) > -1 || lightType.toUpperCase().indexOf(filter) > -1) {
       a[i].style.display = "flex";
+      a[i].classList.add("matchedWithSearch")
+      let totalSearchResult=document.querySelectorAll(".matchedWithSearch")
+      searchResults.innerText=`Totall Results ${totalSearchResult.length}`
+
     } else {
       a[i].style.display = "none";
+      a[i].classList.remove("matchedWithSearch")
     }
-
+   
 
   }
+
 }
 
 // details toggling with index
